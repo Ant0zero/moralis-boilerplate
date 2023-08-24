00e950e7a000f51dacade628f3d3a74e4b6f5004
Ant0zero <139973496+Ant0zero@users.noreply.github.com>
2023-08-24 16:58:18 +0300

    Update Default.tsx

--- src/components/layouts/Default/Default.tsx
+++ src/components/layouts/Default/Default.tsx
@@ -1,6 +1,5 @@
 import { FC, ReactNode } from 'react';
 import { Container } from '@chakra-ui/react';
-import { Header } from 'components/modules';
 import Head from 'next/head';
 
 const Default: FC<{ children: ReactNode; pageName: string }> = ({ children, pageName }) => (
@@ -9,7 +8,6 @@ const Default: FC<{ children: ReactNode; pageName: string }> = ({ children, page
       <title>{`${pageName}`}</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-    <Header />
     <Container maxW="container.lg" p={3} marginTop={100} as="main" minH="70vh">
       {children}
     </Container>
